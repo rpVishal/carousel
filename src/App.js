b9660608e056a130a5c502e4d17674bb177502f5
Vishal Rajput <99860031+rpVishal@users.noreply.github.com>
2023-04-28 23:30:18 +0530

    Add files via upload
    Please install your node_modules by yourself for this project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Bcarousel from './component/Bcarousel';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <Bcarousel/>
+    </div>
+  );
+}
+
+export default App;
